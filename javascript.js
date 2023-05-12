7e17be59e4952479c725b900d579ac24a6ee7e1e
trainboy <unanimoususer99@gmail.com>
2023-05-11 18:55:21 -0700

    function that takes user input

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 
-
-//make a button at the end of the form to take this place //
-// let addButton = document.querySelector(".add-media"); //
-
 const formDisplayBtn = document.querySelectorAll("[data-form-target]");
 const formCloseBtn = document.querySelectorAll("[data-close-button]");
 const backgroundShadow = document.getElementById("background-shadow");
 const entry = document.querySelector(".entry");
+let movieSelect = document.querySelector("input[value='movie']");
+const movieSelected = document.getElementById("m-director");
+const bookSelected = document.getElementById("m-author");
 
 let library = [];
 let movies = [];
@@ -17,18 +16,19 @@ function Media(type, title, year, author, director) {
     this.title = title;
     this.year = year;
     if (type === "movie") {
+        this.author = null;
         this.director = director;
     } else {
         this.author = author;
+        this.director = null;
     };
 };
 
 const entry1 = new Media("movie", "A weekend at malibu", "1999", null, "sardine mccobb");
-
 const entry2 = new Media("book", "the phantom dooker", "2003", "sardine mccobb");
-const entry3 = new Media("book", "lovelust", "2066", "sardine mccobb", );
 
 console.log(entry1, entry2)
+// console.log(entry1.type)
 
 formDisplayBtn.forEach(button => {
     button.addEventListener("click", () => {
@@ -63,24 +63,55 @@ function closeForm(form) {
     backgroundShadow.classList.remove("active");
 };
 
-let eep = 0;
+function mediaInputVisibility() {
+    if (movieSelect.checked) {
+        console.log("movie")
+        bookSelected.classList.add("ns");
+        movieSelected.classList.remove("ns");
+    } else {
+        console.log("book")
+        movieSelected.classList.add("ns");
+        bookSelected.classList.remove("ns");
+    };
+};
+
+movieSelect.addEventListener("change", mediaInputVisibility);
+document.querySelector("input[value='book']").addEventListener("change", mediaInputVisibility);
+
+
 entry.addEventListener("click", ()=>{
-    eep++;
-    console.log(eep)
+    const forms = document.querySelectorAll(".media-form.active");
+    forms.forEach(form => {
+        closeForm(form);
+    });
 });
-console.log(eep);
+// const entry1 = new Media("movie", "A weekend at malibu", "1999", null, "sardine mccobb");
+const data = document.querySelector(".form-body");
 
 
-// addButton.addEventListener('click', ()=>{
-//     if(entry1.type === "movie") {
-//         movies.push(entry1);
-//         movieDisplay(movies);
-//     } else {
-//         books.push(entry1);
-//         bookDisplay();
-//     };
-  
-// });
+
+data.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    
+    const type = data.elements.type.value;
+    const title = data.elements.title.value;
+    const year = data.elements.year.value;
+    const author = data.elements.author.value;
+    const director = data.elements.director.value;
+    const obj = new Media(type, title, year, author, director);
+    
+    library.push(obj);
+    console.log(library);
+
+    sort(obj)
+    data.reset();
+});
+
+function sort(obj){
+    console.log(obj.type)
+};
+
+
 
 
 function movieDisplay(movies) {
